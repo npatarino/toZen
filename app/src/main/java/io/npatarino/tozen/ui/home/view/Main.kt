6b139dc7730da340f8dd6920b5d0d98c1af0cd74
Nicolás Patarino <npatarino@gmail.com>
2018-06-25 18:10:46 +0200

    Add ripple and fic the height of the recyclerview

--- app/src/main/java/io/npatarino/tozen/ui/home/view/Main.kt
+++ app/src/main/java/io/npatarino/tozen/ui/home/view/Main.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import io.npatarino.tozen.R
